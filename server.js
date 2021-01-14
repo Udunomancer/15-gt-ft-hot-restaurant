eda2fda3e1836faffe1298a689392d6ce2b83f5c
Kyle Chriscoe <kyle717@gmail.com>
2021-01-14 13:20:29 -0500

    pseudocoding

---
+++ server.js
@@ -0,0 +1,24 @@
+// ===Require Express===
+const express = require("express");
+
+// ===Create instance of express===
+
+// ===Create a port for the app to run===
+
+// ===Add middleware===
+
+// ===Arrays for Tables and Waitlist===
+
+// ===HTML Routes===
+// ---Home---
+
+// ---Tables---
+
+// ---Reserve---
+
+// ===API Routes===
+// ---Get all table data---
+
+// ---Create a table/reservation---
+
+// ===Listen on the Port===
